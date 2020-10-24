9eba1868bff44b9e591dcc4f9752a4e26337c381
“godfunc” <godfunc@outlook.com>
2020-10-24 09:53:52 +0800

    add test

---
+++ src/com/godfunc/lock/LockSupportTest.java
@@ -0,0 +1,27 @@
+package com.godfunc.lock;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author godfunc
+ * @date 2020/10/23
+ */
+public class LockSupportTest {
+
+    @Test
+    public void test1() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            System.out.println("线程开始执行");
+            LockSupport.park(); // park
+            System.out.println("线程执行结束");
+        });
+        t1.start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("开始唤醒线程");
+         LockSupport.unpark(t1); // unpark
+        t1.join();
+    }
+}
